7873dfcd2b3cd83dd666737a7d82438ec93a9a71
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-04-11 13:03:40 +0530

    implement getDiseases and getDiseaseById methods and map them

--- pathology-service/src/main/java/com/microservice/pathologyservice/resources/PathologyResource.java
+++ pathology-service/src/main/java/com/microservice/pathologyservice/resources/PathologyResource.java
@@ -3,14 +3,28 @@ package com.microservice.pathologyservice.resources;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservice.pathologyservice.models.Disease;
 
 @RestController
+@RequestMapping("/pathology")
 public class PathologyResource {
 
 	List<Disease> diseases = Arrays.asList(
 			new Disease("D1", "Ashma", "Warm water bath"),
 			new Disease("D2", "Thyphoid", "Ampicilin capsule!"));
+	
+	@RequestMapping("/diseases")
+	public List<Disease> getDiseases() {
+		return diseases;
+	}
+	
+	@RequestMapping("/diseases/{id}")
+	public Disease geDiseaseById(@PathVariable("id") String Id) {
+		Disease d = diseases.stream().filter(disease -> Id.equals(disease.getId())).findAny().orElse(null);
+		return d;
+	}
 }
