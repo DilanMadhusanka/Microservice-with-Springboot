93ac859e371f7537c0c5c40ec8a4aa2e438ae3be
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-04-11 17:48:27 +0530

    create getPhisicians method for rest call to hr-resources micro service

--- admission-service/src/main/java/com/mcroser/admissionservice/resources/AdmissionResource.java
+++ admission-service/src/main/java/com/mcroser/admissionservice/resources/AdmissionResource.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,10 @@ public class AdmissionResource {
 
 	List<Patient> patients = Arrays.asList(new Patient("P1", "Rajapaksha", "SriLanka"),
 			new Patient("P2", "Jack", "America"), new Patient("P3", "Tom", "England"));
+	
+	public EmployeeList getPhysicians() {
+		EmployeeList employeesList = restTemplate.getForEntity("http://localhost/hr/employees", EmployeeList.class);
+	}
 
 	@RequestMapping("/patients")
 	public List<Patient> getPatient() {
