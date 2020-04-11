3a2a29b657eaaa02ba9402ef5cfef2601f1ed466
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-04-11 12:41:18 +0530

    implement getEmplyees() and getEmployeeById() methods and map them

--- hr-service/src/main/java/com/microservice/hrservice/resources/HrResources.java
+++ hr-service/src/main/java/com/microservice/hrservice/resources/HrResources.java
@@ -3,6 +3,7 @@ package com.microservice.hrservice.resources;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,17 @@ import com.microservice.hrservice.models.Employee;
 @RequestMapping("/hr")
 public class HrResources {
 
-	List<Employee> employees = Arrays.asList(
-			new Employee("E1", "Kindson", "Munonye", "MediTec"),
-			new Employee("E2", "Lila", "Hardcastle", "Surgery"),
-			new Employee("E3", "Solace", "Chucks", "Dentistry"));
+	List<Employee> employees = Arrays.asList(new Employee("E1", "Kindson", "Munonye", "MediTec"),
+			new Employee("E2", "Lila", "Hardcastle", "Surgery"), new Employee("E3", "Solace", "Chucks", "Dentistry"));
+
+	@RequestMapping("/employees")
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	@RequestMapping("/employees/{id}")
+	public Employee getEmployeeById(@PathVariable("id") String Id) {
+		Employee e = employees.stream().filter(employee -> Id.equals(employee.getId())).findAny().orElse(null);
+		return e;
+	}
 }
