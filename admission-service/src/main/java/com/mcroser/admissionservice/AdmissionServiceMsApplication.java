b854d4ce1d18d4ddf884a883bdc6bb03f158454b
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-04-11 17:20:44 +0530

    Configutre RestTemplate by creating getRestTemplate method in AdmmissionServiceApplication class and create RestTemplate private varable

--- admission-service/src/main/java/com/mcroser/admissionservice/AdmissionServiceMsApplication.java
+++ admission-service/src/main/java/com/mcroser/admissionservice/AdmissionServiceMsApplication.java
@@ -2,9 +2,16 @@ package com.mcroser.admissionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class AdmissionServiceMsApplication {
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdmissionServiceMsApplication.class, args);
--- admission-service/src/main/java/com/mcroser/admissionservice/resources/AdmissionResource.java
+++ admission-service/src/main/java/com/mcroser/admissionservice/resources/AdmissionResource.java
@@ -3,15 +3,20 @@ package com.mcroser.admissionservice.resources;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.mcroser.admissionservice.resources.models.Patient;
 
 @RestController
 @RequestMapping("/admissions")
 public class AdmissionResource {
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	List<Patient> patients = Arrays.asList(new Patient("P1", "Rajapaksha", "SriLanka"),
 			new Patient("P2", "Jack", "America"), new Patient("P3", "Tom", "England"));
