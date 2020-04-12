36af30a943ecf7de90ab501e49687cd415a159a9
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-04-12 18:30:26 +0530

    remove unwnated zip file and add relevent dependency, dependencyManagement,respository and cloud version to the client server(micorservices)

--- admission-service/src/main/java/com/mcroser/admissionservice/AdmissionServiceMsApplication.java
+++ admission-service/src/main/java/com/mcroser/admissionservice/AdmissionServiceMsApplication.java
@@ -2,10 +2,12 @@ package com.mcroser.admissionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AdmissionServiceMsApplication {
 	
 	@Bean
